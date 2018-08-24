3d22ff70018d7fcad5f8d0ad43682ef4973b1e13
Priya312 <py10089@gmail.com>
2018-08-25 02:27:08 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/background/sync/WaterReminderFirebaseJobService.java
@@ -0,0 +1,38 @@
+package com.example.android.background.sync;
+
+import com.firebase.jobdispatcher.JobParameters;
+import com.firebase.jobdispatcher.JobService;
+import android.content.Context;
+import android.os.AsyncTask;
+
+public class WaterReminderFirebaseJobService extends JobService {
+
+    private AsyncTask mBackgroundTask;
+
+    @Override
+    public boolean onStartJob(final JobParameters params) {
+        mBackgroundTask = new AsyncTask() {
+            @Override
+            protected Object doInBackground(Object[] objects) {
+                Context context = WaterReminderFirebaseJobService.this;
+                ReminderTasks.executeTask(context, ReminderTasks.ACTION_CHARGING_REMINDER);
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Object o) {
+                jobFinished(params, false);
+            }
+        };
+
+        mBackgroundTask.execute();
+        return true;
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters params) {
+        if (mBackgroundTask != null)
+            mBackgroundTask.cancel(true);
+        return true;
+    }
+}
